16967a7b5e67aaaeb63306dff1142ce26ba5a548
lucaszeng <lucaszeng99@gmail.com>
2018-08-15 21:57:45 -0600

    fetch locale storage at startup. Todo: rebust

--- renderer/main.js
+++ renderer/main.js
@@ -5,12 +5,75 @@ let buttonMinimize = document.getElementById('_')
 let buttonAddList = document.getElementById('list-add')
 let buttonAddItem = document.getElementById('item-add')
 
-localStorage.clear()
 let targetList = document.getElementById('today')
 let today = targetList
 
 let listsContainer = document.getElementById('list-names')// li
 let itemsContainer = document.getElementById('items') // li
+
+console.log(Object.keys(localStorage))
+let storeCurrentList = () => {
+  // create object to be stored.
+  let items = document.getElementsByClassName('item')
+  let itemsArray = []
+  for (item of items) {
+    itemsArray.push(item.id)
+  }
+  localStorage.setItem(targetList.id, JSON.stringify(itemsArray))
+}
+
+let changeTarget = (newTargetElement) => {
+  targetList = newTargetElement
+  // remove all current items of the old list
+  while (itemsContainer.firstChild) {
+    itemsContainer.removeChild(itemsContainer.firstChild)
+  }
+  // fetch previous items from local storage
+  itemsStringFromLocalStorage = localStorage.getItem(targetList.id)
+  let itemsArray = JSON.parse(itemsStringFromLocalStorage)
+  if (itemsArray !== null) {
+    let childs = itemsArray.map(item => {
+      let x = document.createElement('ul')
+      x.id = item
+      x.innerText = item
+      x.className = 'item'
+      return x
+    })
+    for (let item of childs) {
+      itemsContainer.appendChild(item)
+    }
+  }
+  
+}
+let fetchItems = () => {
+  let items = localStorage.getItem(targetList.id)
+  items = JSON.parse(items)
+  for (let item of items) {
+    let newItem = document.createElement('ul')
+    newItem.classList.add('item')
+    newItem.id = item
+    newItem.textContent = item
+
+    itemsContainer.appendChild(newItem)
+  }
+}
+let fetchList = () => {
+  let lists = Object.keys(localStorage)
+  for (let listId of lists) {
+    let newList = document.createElement('ul')
+    newList.classList.add('list-name')
+    newList.id = listId
+    newList.textContent = listId
+    newList.addEventListener('click', () => {
+      changeTarget(newList)
+    })
+    // append this child to the list of lists.
+    listsContainer.appendChild(newList)
+  }
+}
+
+fetchList()
+fetchItems()
 
 today.addEventListener('click', () => {
   changeTarget(today)
@@ -54,12 +117,12 @@ buttonAddItem.addEventListener('click', () => {
   inputElement.addEventListener('keydown', event => {
     if (event.key === 'Enter') {
       // inserting one more list ul.
-      let newList = document.createElement('ul')
-      newList.classList.add('item')
-      newList.id = inputElement.value
-      newList.textContent = inputElement.value
+      let newItem = document.createElement('ul')
+      newItem.classList.add('item')
+      newItem.id = inputElement.value
+      newItem.textContent = inputElement.value
 
-      itemsContainer.appendChild(newList)
+      itemsContainer.appendChild(newItem)
       // destruct input element
       inputElement.remove()
       storeCurrentList()
@@ -68,38 +131,4 @@ buttonAddItem.addEventListener('click', () => {
   buttonAddItem.insertAdjacentElement('beforebegin', inputElement)
 })
 
-
-let storeCurrentList = () => {
-  // create object to be stored.
-  let items = document.getElementsByClassName('item')
-  let itemsArray = []
-  for (item of items) {
-    itemsArray.push(item.id)
-  }
-  localStorage.setItem(targetList.id, JSON.stringify(itemsArray))
-}
-
-let changeTarget = (newTargetElement) => {
-  targetList = newTargetElement
-  // remove all current items of the old list
-  while (itemsContainer.firstChild) {
-    itemsContainer.removeChild(itemsContainer.firstChild)
-  }
-  // fetch previous items from local storage
-  itemsStringFromLocalStorage = localStorage.getItem(targetList.id)
-  let itemsArray = JSON.parse(itemsStringFromLocalStorage)
-  if (itemsArray !== null) {
-    let childs = itemsArray.map(item => {
-      let x = document.createElement('ul')
-      x.id = item
-      x.innerText = item
-      x.className = 'item'
-      return x
-    })
-    for (let item of childs) {
-      itemsContainer.appendChild(item)
-    }
-  }
-  
-}
 console.log(localStorage)
