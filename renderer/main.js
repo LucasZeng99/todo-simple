17c5213940bb8b4778a3594337ea7572f3563761
lucaszeng <lucaszeng99@gmail.com>
2018-08-15 23:02:41 -0600

    fix repetition, right click to remove item. TODO: right click to remove list, style up.

--- renderer/main.js
+++ renderer/main.js
@@ -5,13 +5,31 @@ let buttonMinimize = document.getElementById('_')
 let buttonAddList = document.getElementById('list-add')
 let buttonAddItem = document.getElementById('item-add')
 
+// options for the app
+buttonClose.addEventListener('click', () => ipcRenderer.send('close'))
+buttonMinimize.addEventListener('click', () => ipcRenderer.send('minimize'))
+
 let targetList = document.getElementById('today')
-let today = targetList
 
 let listsContainer = document.getElementById('list-names')// li
 let itemsContainer = document.getElementById('items') // li
 
 console.log(Object.keys(localStorage))
+let createItem = (val) => {
+  let newItem = document.createElement('ul')
+      newItem.classList.add('item')
+      newItem.id = val
+      newItem.textContent = val
+
+      // listeners for every item of a list.
+      newItem.addEventListener('mouseup', e => {
+        if (e.button === 2) { // a right click.
+          newItem.remove() // remove the item
+          storeCurrentList()
+        }
+      })
+  return newItem
+}
 let storeCurrentList = () => {
   // create object to be stored.
   let items = document.getElementsByClassName('item')
@@ -24,19 +42,18 @@ let storeCurrentList = () => {
 
 let changeTarget = (newTargetElement) => {
   targetList = newTargetElement
+
   // remove all current items of the old list
   while (itemsContainer.firstChild) {
     itemsContainer.removeChild(itemsContainer.firstChild)
   }
+
   // fetch previous items from local storage
   itemsStringFromLocalStorage = localStorage.getItem(targetList.id)
   let itemsArray = JSON.parse(itemsStringFromLocalStorage)
   if (itemsArray !== null) {
     let childs = itemsArray.map(item => {
-      let x = document.createElement('ul')
-      x.id = item
-      x.innerText = item
-      x.className = 'item'
+      let x = createItem(item)
       return x
     })
     for (let item of childs) {
@@ -48,17 +65,17 @@ let changeTarget = (newTargetElement) => {
 let fetchItems = () => {
   let items = localStorage.getItem(targetList.id)
   items = JSON.parse(items)
-  for (let item of items) {
-    let newItem = document.createElement('ul')
-    newItem.classList.add('item')
-    newItem.id = item
-    newItem.textContent = item
-
+  for (let itemId of items) {
+    let newItem = createItem(itemId)
     itemsContainer.appendChild(newItem)
   }
 }
 let fetchList = () => {
   let lists = Object.keys(localStorage)
+  // remove all list to avoid repetance
+  while (listsContainer.firstChild) {
+    listsContainer.removeChild(listsContainer.firstChild)
+  }
   for (let listId of lists) {
     let newList = document.createElement('ul')
     newList.classList.add('list-name')
@@ -66,7 +83,7 @@ let fetchList = () => {
     newList.textContent = listId
     newList.addEventListener('click', () => {
       changeTarget(newList)
-    })
+    })    
     // append this child to the list of lists.
     listsContainer.appendChild(newList)
   }
@@ -74,13 +91,6 @@ let fetchList = () => {
 
 fetchList()
 fetchItems()
-
-today.addEventListener('click', () => {
-  changeTarget(today)
-})
-
-buttonClose.addEventListener('click', () => ipcRenderer.send('close'))
-buttonMinimize.addEventListener('click', () => ipcRenderer.send('minimize'))
 
 buttonAddList.addEventListener('click', () => {
   let inputElement = document.createElement('input')
@@ -117,11 +127,7 @@ buttonAddItem.addEventListener('click', () => {
   inputElement.addEventListener('keydown', event => {
     if (event.key === 'Enter') {
       // inserting one more list ul.
-      let newItem = document.createElement('ul')
-      newItem.classList.add('item')
-      newItem.id = inputElement.value
-      newItem.textContent = inputElement.value
-
+      let newItem = createItem(inputElement.value)
       itemsContainer.appendChild(newItem)
       // destruct input element
       inputElement.remove()
